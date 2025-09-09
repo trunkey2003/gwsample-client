646af638e95501371d48a3ec0fa84d828bed2569
Lam Ho Quang <21521049@gm.uit.edu.vn>
2025-09-09 07:47:42 +0000

    feat: render simple page

--- webapp/controller/GWSample-Client.controller.js
+++ webapp/controller/GWSample-Client.controller.js
@@ -1,12 +1,16 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
-],
-function (Controller) {
+], function (Controller) {
     "use strict";
 
     return Controller.extend("ns.gwsampleclient.controller.GWSample-Client", {
         onInit: function () {
+            // Initialize the view
+        },
 
+        onNavigateToSalesOrders: function () {
+            var oRouter = this.getOwnerComponent().getRouter();
+            oRouter.navTo("SalesOrderList");
         }
     });
 });
